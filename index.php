a2b835ae95e7abd221496a7ca3f97c9455935d3e
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-07-15 18:32:39 +0530

    update1

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 </head>
 <body>
 <div id="space">
+<h1 id="heading">ResumeMaker </h1>
+
+<div class="data">
+<div class="info">
+<input type="text" id="name" name="name"  placeholder="your name"  autocomplete="on" required>
+<input type="text" id="degree" name="degree"  placeholder="qualification   e.g B.tech cse"  autocomplete="on" required> <br>
+<input type="email" id="email" name="email"  placeholder="your email-id"  autocomplete="on" required>
+<input type="number" id="phone" name="phone"  placeholder="phone-no"  required> <br>
+<input type="date" name="dob" id="dob" placeholder="birth date"  required>
+<input type="text" id="college" name="college"  placeholder="college name"  autocomplete="on" required><br>
+
+</div>
+
+
+
+	
+</div>
 	
 
 </div>
