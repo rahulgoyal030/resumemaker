9cec4434fac4565e36b8239a1b85c8e735f4cad8
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-07-15 21:04:44 +0530

    form changes

--- index.php
+++ index.php
@@ -17,13 +17,40 @@
 <input type="number" id="phone" name="phone"  placeholder="phone-no"  required> <br>
 <input type="date" name="dob" id="dob" placeholder="birth date"  required>
 <input type="text" id="college" name="college"  placeholder="college name"  autocomplete="on" required><br>
-</div>
+</div><br>
 
 
-<p id="objectiveHeading">objective :- </p>
-<div class="objective">
+<p class="infoHeading">objective :- </p>
+<div class="infoArea">
 <textarea rows="6" cols="85" name="objective" id="objectiveText" placeholder=" write your objective here ">
 </textarea>
+</div>
+<br><br><br><br><br>
+
+<p class="infoHeading">Skill Set :-</p>
+<div class="infoArea">
+<textarea rows="8" cols="85" name="skill" id="skillText" placeholder=" write your skills here ">
+</textarea>
+</div>
+<br><br><br><br><br><br><br>
+
+<p class="infoHeading">Areas of interest :- </p>
+<div class="infoArea">
+<textarea rows="6" cols="85" name="interest" id="interestText" placeholder=" write your areas of interests here ">
+</textarea>
+</div>
+<br><br><br><br><br><br>
+<p class="infoHeading">Projects :-</p>
+<div class="infoArea">
+<input type="text" class="projectName" name="project"  placeholder="project heading"   required>
+<textarea rows="8" cols="95" name="projectDetail" class="projectDetail" placeholder=" write projects details here  ">
+</textarea>
+<div class="newProjects"></div>
+<label class="newProjectHeading">add a new project  :-</label> 
+<button type="submit"  id="addNew">new project</button>
+</div>
+
+	
 </div>
 
 </div>
