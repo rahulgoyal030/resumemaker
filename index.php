2abdfad036d28b09d16d306b3e96d88d8c1dafca
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-07-15 17:33:03 +0530

    ist commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>resume maker</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="space">
+	
+
+</div>
+
+</body>
+</html>
