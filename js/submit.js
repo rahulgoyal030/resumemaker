d24ee230ca6a62a9c9b8ac2063f1cb985c359c7e
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-07-19 00:35:53 +0530

    changes in submision

--- js/submit.js
+++ js/submit.js
@@ -1,11 +1,19 @@
 $(document).ready(function  () {
 
 
- 
+
 
    $("#download").click(function(event){
    	event.preventDefault();
    	window.location = "download.php";
+
+
+    
+});
+
+   $("#addNew").click(function(event){
+   	event.preventDefault();
+   
 
 
     
